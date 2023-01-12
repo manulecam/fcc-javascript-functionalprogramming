232be552b9c4ba2bbb0a0f04eebd9efa789410d3
manulecam <lecam.manuel@gmail.com>
2023-01-12 02:52:15 -0300

    Commit

---
+++ devolver-parte-de-un-arreglo-mediante-el-mtodo-slice.js
@@ -0,0 +1,9 @@
+function sliceArray(anim, beginSlice, endSlice) {
+  // Cambia solo el código debajo de esta línea
+  let newArray = anim.slice(beginSlice, endSlice);
+  return newArray;
+  // Cambia solo el código encima de esta línea
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
